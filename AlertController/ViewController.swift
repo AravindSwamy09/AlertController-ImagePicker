550d36d789363317efed31befac41c90a01b9445
ESS Mac Pro <ESSMacPro@esss-macbook-pro.local>
2017-03-31 08:03:12 -0400

    Image Picker : 31/03/2017

--- AlertController/ViewController.swift
+++ AlertController/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -41,11 +42,37 @@ class ViewController: UIViewController {
     
     @IBAction func actionSheet(_ sender: Any) {
         
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        
         let alertSheet = UIAlertController(title: "Image Source", message: "Choose camera or your Photo library", preferredStyle: .actionSheet)
-        alertSheet.addAction(UIAlertAction(title: "Camera", style: .default))
-        alertSheet.addAction(UIAlertAction(title: "Photo Library", style: .default))
+        alertSheet.addAction(UIAlertAction(title: "Camera", style: .default){(action:UIAlertAction) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+                picker.sourceType = .camera
+                self.present(picker, animated: true, completion: nil)
+            }else{
+                let alert = UIAlertController(title: "Message", message: "Camera is not available.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        })
+        alertSheet.addAction(UIAlertAction(title: "Photo Library", style: .default){(action:UIAlertAction) in
+            picker.sourceType = .photoLibrary
+            self.present(picker, animated: true, completion: nil)
+        })
         alertSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         present(alertSheet, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = image
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
     
     override func didReceiveMemoryWarning() {
